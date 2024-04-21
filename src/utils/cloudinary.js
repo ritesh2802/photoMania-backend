2269edaf9433a81030eedaab8701fbcd59a181d8
ritesh2802 <singhritesh02000@gmail.com>
2024-04-21 21:53:02 +0530

    cloudinary env modified

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,15 @@
 import {v2 as cloudinary} from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './.env'
+})
 
 cloudinary.config({
-    cloud_name: 'djxigze5l', 
-    api_key: '287459617835886', 
-    api_secret: 'JyGYgz_3mSrbr4p__1cfXPgXZII'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 const uploadOnCloudinary =async (localFilePath)=>{
