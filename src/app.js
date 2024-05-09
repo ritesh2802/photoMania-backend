5c2d26d9acb4d450ad65e3dc21487b26e17eddf3
ritesh2802 <singhritesh02000@gmail.com>
2024-05-09 12:30:57 +0530

    methodes

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credetials:true,
-    methods:["GET","POST","PATCH","DELETE"]
+    
 }))
 
 // app.use(function(req, res, next) {
